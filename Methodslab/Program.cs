733c39a18d394eff04c8e5f7ef008c8a75c33f2e
John Doe <gogs@fake.local>
2021-12-08 09:59:13 +0200

    Добавьте файлы проекта.

---
+++ Methodslab/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Methodslab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = Func(1, 4) + Func(7, 5) + Func(3,2);
+            Console.WriteLine("y={0:F2}",y);
+            try
+            {
+                Console.WriteLine("Введите число в 8 c/c:");
+                string ch;
+                int k = 0;
+                do
+                {
+                    ch = Console.ReadLine();
+                    k = 0;
+                    for (int i = 0; i < ch.Length; i++)
+                    {
+                        if (Char.IsDigit(ch[i])&&((int.Parse(ch[i].ToString()) < 0) ||
+                                int.Parse(ch[i].ToString()) > 7))
+                            {
+                                k++;
+                            }
+                    }
+                    if (k > 0) Console.WriteLine("Введите корректные числа:");
+                }
+                while (k>0);
+                Console.WriteLine(toDec(ch));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //5.2 - базовый 
+            int[,] mas = new int[6, 4];
+            Random rnd = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    mas[i, j] = rnd.Next(-10, 11);
+                    Console.Write(mas[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            
+            int[] res = MyLib.ClassLib.SumNegative(mas);
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.Write(res[i]+" ");
+            }
+            Console.WriteLine();
+        }
+        static double Func(int x,int y)
+        {
+            return (x+Math.Sin(y)) / (Math.Sin(x)+y);
+        }
+        static int toDec(string str)
+        {
+            int s = 0;
+            int step = str.Length - 1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                s += int.Parse(str[i].ToString()) * (int)Math.Pow(8, step);
+                step--;
+            }
+            return s;
+        }
+    }
+}
---
+++ MyLib/ClassLib.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class ClassLib
+    {
+        public static int[] SumNegative(int[,] mas)
+        {
+            int rows = mas.GetUpperBound(0) + 1;    // количество строк
+            int columns = mas.Length / rows;
+            int[] res = new int[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                int S = 0;
+                for (int j = 0; j < columns; j++)
+                {
+                    if (mas[i, j] < 0) S += mas[i, j];
+                }
+                res[i] = S;
+            }
+            return res;
+        }
+    }
+}
